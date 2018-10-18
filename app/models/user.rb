ba17eecdb6a15765bd54c4ab827a59883466de04
romenigld <romenigld@gmail.com>
2018-10-18 17:56:26 +0200

    Stopped at page 97

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
+  has_many :memberships
+  has_many :accounts, through: :memberships
+
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def owned_accounts
+    Account.where(owner: self)
+  end
+
+  def all_accounts
+    owned_accounts + accounts
+  end
 end
